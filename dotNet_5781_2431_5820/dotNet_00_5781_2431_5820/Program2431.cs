0394927749f4c1ef24c8a8506204ce6052eced8c
אפרת פריד <efried6188@gmail.com>
2020-10-25 21:39:32 +0200

    this is the first h.w of "mini project in windows" cours. we changed the title of the program ,and made a class program.

---
+++ dotNet_5781_2431_5820/dotNet_00_5781_2431_5820/Program2431.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_00_5781_2431_5820
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2431();
+            Welcome5820();
+            Console.ReadKey();
+        }
+
+        private static void Welcome2431()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        private static void Welcome5820()
+        {
+        }
+        
+    }
+}

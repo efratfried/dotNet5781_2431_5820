f296a099ce38f7d23e1c91d01fd50f3ccbc751e0
tamar tamar <tamarpackter1@gmail.com>
2020-10-25 23:07:27 +0200

    i added a new class

---
+++ dotNet_5781_2431_5820/dotNet_00_5781_2431_5820/Program5820.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_00_5781_2431_5820
+{
+    partial class Program
+    {
+        static partial void Welcome5820()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}

7ec82730dbd101868a24cd2bed9816f5d087250c
tamar tamar <tamarpackter1@gmail.com>
2020-11-05 01:19:55 +0200

    start

---
+++ dotNet_5781_2431_5820/dotNet_01_5781_2431_5820/dotNet_01_5781_2431_5820/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_01_5781_2431_5820
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 1;
+        }
+    }
+}

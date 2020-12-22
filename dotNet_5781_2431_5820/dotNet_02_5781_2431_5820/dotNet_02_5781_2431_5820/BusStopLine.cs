84b427cef0bbe913f91c9d452a15a9aba94de8f0
אפרת פריד <efried6188@gmail.com>
2020-12-22 23:21:44 +0200

    this is the worst thing i have ever had to do on will

--- dotNet_5781_2431_5820/dotNet_02_5781_2431_5820/dotNet_02_5781_2431_5820/BusStopLine.cs
+++ dotNet_5781_2431_5820/dotNet_02_5781_2431_5820/dotNet_02_5781_2431_5820/BusStopLine.cs
@@ -10,9 +10,9 @@ using System.Threading.Tasks;
 using System.Xml.Schema;
 namespace dotNet_02_5781_2431_5820.git
 {
-    public class BusStopLine : BusStop, IComparable, IEnumerator
+    public class BusStopLine : BusStop
     {
-        private int iCurrent = -1;
+        
         public BusStopLine() : base()
         {
 
@@ -32,58 +32,6 @@ namespace dotNet_02_5781_2431_5820.git
             int dis = Convert.ToInt32(Dis);//dont care to loose a little bit info because it is not exact but evaluieted time
             TimeSpan dt = new TimeSpan(dis);
             return dt;
-        }
-
-        public int CompareTo(object obj)
-        {
-            if (obj == null) return 1;
-
-            BusLine otherTemperature = obj as BusLine;
-            if (otherTemperature != null)
-                return this.CompareTo(otherTemperature);
-            else
-                throw new ArgumentException("Object is not a Temperature");
-        }
-        public int CompareTo(BusLine A, BusLine B, BusStopLine distanation)//compare which driving time is shorter.
-        {
-            //compare between two stations's code to see which station is first/second.
-            int IndexOfFirst = A.IndexOfStation(this.CodeStation);
-            int IndexOfSecond = B.IndexOfStation(this.CodeStation);
-
-            if (A.DrivingTimeBetweenTwoStations(A.LineStops[IndexOfFirst].CodeStation, distanation.CodeStation) < (B.DrivingTimeBetweenTwoStations(B.LineStops[IndexOfSecond].CodeStation, distanation.CodeStation)))
-            {//if a is amaller than b.
-                return -1;
-            }
-            else if (A.DrivingTimeBetweenTwoStations(A.LineStops[IndexOfFirst].CodeStation, distanation.CodeStation) > (B.DrivingTimeBetweenTwoStations(B.LineStops[IndexOfSecond].CodeStation, distanation.CodeStation)))
-            {//if a is bigger than b.
-                return 1;
-            }
-            else//if a & b are equal.
-                return 0;
-        }
-        public bool MoveNext()
-        {
-            if (iCurrent < LineStops.Count - 1)
-            {
-                ++iCurrent;
-                return true;
-            }
-            return false;
-        }
-        public void Reset()
-        {
-            iCurrent = -1;
-        }
-        public object Current
-        {
-            get
-            {
-                return LineStops[iCurrent];
-            }
-        }
-        public IEnumerator GetEnumerator()
-        {
-            return (IEnumerator)this;
-        }
+        }   
     }
 }

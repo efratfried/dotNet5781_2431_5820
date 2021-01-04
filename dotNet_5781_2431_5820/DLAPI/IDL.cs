bf32e3153a0db19c0eb4437d7b839f88acae3daf
אפרת פריד <efried6188@gmail.com>
2021-01-04 18:53:08 +0200

    i sort the all cruds function accroding to the files names by region

--- dotNet_5781_2431_5820/DLAPI/IDL.cs
+++ dotNet_5781_2431_5820/DLAPI/IDL.cs
@@ -11,74 +11,72 @@ namespace DLAPI
     public interface IDL
     {
         #region Bus
-        void AddBus(int BusNum);
+        int AddBus(int BusNum);
         Bus GetBus(int BusNum);
         void UpdateBus(int BusNum);
         void DeleteBus(int BusNum);
         #endregion
         #region Station
-        void AddStation(int CodeStation);
-        void GetStation(int CodeStation);
+        int AddStation(int CodeStation);
+        Station GetStation(int CodeStation);
         void UpdateStation(int CodeStation);
         void DeleteStation(int CodeStation);
         #endregion
         #region User
-        void AddUser(string Name, int Password);
+        int AddUser(string Name, int Password);
         User GetUserName(int password);
         User GetUserPassword(int PAssword);
         void UpdateUser(string Name, int Password);
         void DeleteUser(string Name, int Password);
         #endregion
         #region Adress
-        void AddAdress(string Street, int num, string city);
+        int AddAdress(string Street, int num, string city);
         void GetAdress(string Street, int num, string city);
         void UpdateAdress(string Street, int num, string city);
         void DeleteAdress(string Street, int num, string city);
         #endregion
-
-        //Create
-
-        void AddBusLine(int BusNum);
-        void AddBusStationLine(int BusNum, int CodeStation);
-        void AddDrivingLine(int BusNum);
-        void AddLocation(double latitude,double longtitude);
-        void AddOutGoingLine(int BusNum);
-        void AddUserDrive(string name,int password);
-
-        //Request
-        
+        #region BusLine
+        int AddBusLine(int BusNum);
         BusLine GetBusLine(int BusNum);
-        BusStationLine GetBusStationLine(int BusNum, int CodeStation);
-        DrivingBus GetDrivingBus(int BusNum);
-        FollowingStations GetFollowingStations(int CodeStation);
-        Location GetLocation(double latitude, double longtitude);
-        OutGoingLine GetOutGoingLine(int BusNum);
-        
-       
-        
-        //Update
-        
-        
-       
-        
         void UpdateBusLine(int BusNum);
-        void UpdateBusStationLine(int BusNum,int CodeStation);
-        void UpdateDrivingLine(int BusNum);
-        void UpdateLocation(double latitude, double longtitude);
-        void UpdateOutGoingLine(int BusNum);
-        void UpdateUserDrive(string name);
-
-        //Remove
-        
-        
-        
-        
         void DeleteBusLine(int BusNum);
-        void DeleteBusStationLine(int BusNum,int CodeStation);
+        #endregion
+        #region BusStationLine
+        int AddBusStationLine(int BusNum, int CodeStation);
+        BusStationLine GetBusStationLine(int BusNum, int CodeStation);
+        void UpdateBusStationLine(int BusNum, int CodeStation);
+        void DeleteBusStationLine(int BusNum, int CodeStation);
+        #endregion
+        #region DrivingLine
+        int AddDrivingLine(int BusNum);
+        DrivingBus GetDrivingBus(int BusNum);
+        void UpdateDrivingLine(int BusNum);
         void DeleteDrivingLine(int BusNum);
+        #endregion
+        #region Location
+        int AddLocation(double latitude, double longtitude);
+        Location GetLocation(double latitude, double longtitude);
+        void UpdateLocation(double latitude, double longtitude);
         void DeleteLocation(double latitude, double longtitude);
+        #endregion
+        #region OutGoingLine
+        int AddOutGoingLine(int BusNum);
+        OutGoingLine GetOutGoingLine(int BusNum);
+        void UpdateOutGoingLine(int BusNum);
         void DeleteOutGoingLine(int BusNum);
+        #endregion
+        #region UserDrive
+        int AddUserDrive(string name, int password);
+        UserDrive GetUserDrive(string Name);
+        void UpdateUserDrive(string name);
         void DeleteUserDrive(string name);
-        
+
+        #endregion
+        #region FollowingStations
+        int AddFolowingStations(int FirstCodeStation, int SecondCodeStation);
+        FollowingStations GetFollowingStations(int FirstCodeStation, int SecondCodeStation);
+        void UpdateFollowingStations(int FirstCodeStation, int SecondCodeStation);
+        void DeleteFollowingStations(int FirstCodeStation, int SecondCodeStation);
+        #endregion
     }
 }

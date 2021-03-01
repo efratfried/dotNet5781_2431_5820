5a68f9f3c08963b3d5c79b85d07679ad8e85a08f
אפרת פריד <efried6188@gmail.com>
2021-03-01 12:37:46 +0200

    add the addbus wondow

---
+++ dotNet_5781_2431_5820/PL/AddBus.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BLAPI;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for AddBus.xaml
+    /// </summary>
+    public partial class AddBus : Window
+    {
+        public BO.Bus addbus;
+        public bool AllFieldsWereFilled = false;
+        public bool thereIsATrip = false;
+        public AddBus()
+        {
+            InitializeComponent();
+        }
+
+        private void AddLineDetails_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddBus_Click(object sender, RoutedEventArgs e)
+        {
+            if (LicenseNum.Text != "")
+            {
+                AllFieldsWereFilled = true;
+                addbus.LicenseNum = LicenseNum.Text;
+                addbus.LicenseDate = DateTime.Parse(LicenseDate.SelectedDate.ToString());
+                addbus.KM = double.Parse(busKM.Text);
+                addbus.foul = double.Parse(busfoul.Text);
+                addbus.Firm = BO.Firm;             
+                addbus.AccidentsDuco = accident.Text;
+            }
+
+            MessageBoxResult res = MessageBox.Show("Add line?", "Verification", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (res == MessageBoxResult.No)
+                return;
+
+            this.Close();
+        }
+    }
+}
